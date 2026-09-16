2e67e22dae34fa9b6b293a957137a3d7a0df2568
Lucas Gómez (KB) <79912231+Pigamer37@users.noreply.github.com>
2026-09-16 18:48:44 +0200

    Clear `seen` Set between stream types (#29)

--- addon.js
+++ addon.js
@@ -177,6 +177,8 @@ async function extractOkRuStreams(iframeSrc) {
             }
         }));
 
+    seen.clear();
+
     const proxied = (info.formats || [])
         .filter(f => f.protocol === 'm3u8_native' && f.url && f.height)
         .sort((a, b) => (b.height || 0) - (a.height || 0))
